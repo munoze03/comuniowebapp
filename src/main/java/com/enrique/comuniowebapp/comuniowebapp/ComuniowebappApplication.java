2e9f4d16af9475a34a23e745bbc4f6b39afabe21
Enrique <munoze03@gmail.com>
2025-07-26 17:38:30 +0200

    Creacion de Proyecto

---
+++ src/main/java/com/enrique/comuniowebapp/comuniowebapp/ComuniowebappApplication.java
@@ -0,0 +1,13 @@
+package com.enrique.comuniowebapp.comuniowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComuniowebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComuniowebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enrique/comuniowebapp/comuniowebapp/ComuniowebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enrique.comuniowebapp.comuniowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComuniowebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
