f595a40277dc3af8a43d7381d3c39c4d5055e0a1
Enrique <munoze03@gmail.com>
2025-09-29 17:35:45 +0200

    cambio en la alineacion de jugador, imagen de escudo en vez de nombre

--- src/main/resources/static/js/scriptClasificacion.js
+++ src/main/resources/static/js/scriptClasificacion.js
@@ -268,9 +268,19 @@ async function renderAlineacionJugador(userId, containerId) {
                 row.appendChild(tdNombre)
 
 
-                const tdClub = document.createElement("td");
-                tdClub.textContent = plantillaJugador[i].club;
-                row.appendChild(tdClub)
+                const tdFotoClub = document.createElement("td");
+                // Crear el elemento <img>
+                const imgClub = document.createElement("img");
+                imgClub.src = plantillaJugador[i].hrefClubLogo; // URL de la imagen
+                imgClub.alt = plantillaJugador[i].club;
+                imgClub.className = "rounded-circle fotoPlantilla";   
+                imgClub.style.width = "30px";               // ancho opcional
+                imgClub.style.height = "30px";              // alto opcional
+                imgClub.style.objectFit = "cover";          // para que no se deforme
+
+                // Añadir la imagen a la celda
+                tdFotoClub.appendChild(imgClub);
+                row.appendChild(tdFotoClub);
 
                 const tdPuntos = document.createElement("td");
                 if(plantillaJugador[i].ultimosPuntos != ""){
@@ -308,9 +318,19 @@ async function renderAlineacionJugador(userId, containerId) {
                     row.appendChild(tdNombre)
 
 
-                    const tdClub = document.createElement("td");
-                    tdClub.textContent = plantillaJugador[i].club;
-                    row.appendChild(tdClub)
+                    const tdFotoClub = document.createElement("td");
+                    // Crear el elemento <img>
+                    const imgClub = document.createElement("img");
+                    imgClub.src = plantillaJugador[i].hrefClubLogo; // URL de la imagen
+                    imgClub.alt = plantillaJugador[i].club;
+                    imgClub.className = "rounded-circle fotoPlantilla";   
+                    imgClub.style.width = "30px";               // ancho opcional
+                    imgClub.style.height = "30px";              // alto opcional
+                    imgClub.style.objectFit = "cover";          // para que no se deforme
+
+                    // Añadir la imagen a la celda
+                    tdFotoClub.appendChild(imgClub);
+                    row.appendChild(tdFotoClub);
 
                     const tdPuntos = document.createElement("td");
                     if(plantillaJugador[i].puntosTotales != ""){
